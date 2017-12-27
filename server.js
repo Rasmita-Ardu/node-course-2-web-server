da8348e14be3ed994ee9391e2ed0fb3e03669302
Rasmita-Ardu <rasmitaardu97@gmail.com>
2017-12-27 19:31:25 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require ('express');
+const hbs = require ('hbs');
+const fs = require ('fs');
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('View engine','hbs');
+app.use(express.static(__dirname + '/public')); //middleware
+
+app.use((req,res,next)=>{
+	var now = new Date().toString();
+	var log = `${now} : ${req.method} ${req.url}`;
+	fs.appendFile('server.log',log +'\n',(err)=> {
+	if (err) console.log('unable to print');})
+	next();
+});
+app.use((req,res,next)=>{
+	res.render('maintainance.hbs',{
+		pageTitle : 'Maintainance',
+		message : 'Site is being updated !'
+	});
+});
+
+hbs.registerHelper('getCurrentYear',()=>{
+	return new Date().getFullYear()
+});
+hbs.registerHelper('screamIt',(text)=>{
+	return text.toUpperCase()
+});
+
+app.get('/',(req,res)=>{
+	//res.send('<h1><i>Hello Express!</i></h1>');
+	// res.send ({
+		// name : 'Rasmita',
+		// likes :['sleeping',
+		         // 'shoppng']
+	// });
+	res.render('home.hbs',{
+		pageTitle : 'home page',
+		message : 'Welcome here'
+	});
+	
+});
+
+app.get('/about',(req,res) =>{
+	//res.send('About Page');
+	res.render('about.hbs',{
+		pageTitle : 'About page',
+		});
+});
+app.get('/bad',(req,res) =>{
+	res.send({
+	error :'error handling request'});
+});
+
+app.listen(3000,()=>{
+	console.log('Server is up on port 3000');
+});
